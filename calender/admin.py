6bf43fbf4dc6ca92b6c535699f6ed3bcc1a1e495
starkvr1 <prateekpatriotasme@gmail.com>
2022-04-17 11:48:06 +0530

    updated commit

--- calender/admin.py
+++ calender/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import celenderEvent
 # Register your models here.
+
+admin.site.register(celenderEvent)
--- calender/models.py
+++ calender/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+appid=1
+class celenderEvent(models.Model):
+ 
+    event_name=models.CharField(max_length=100,default=f'Appointment {appid}')
+    appid+=1
+    catogory=models.CharField(max_length=50)
+    priorty=models.CharField(max_length=50)
+    eventTYPE=models.CharField(max_length=50)
